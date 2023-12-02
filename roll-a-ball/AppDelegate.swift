5d205465bdba6e83fe33ceb47b5d9bd6eb851e9b
Deep Shah <deepshah11111@gmail.com>
2023-12-02 11:23:30 -0800

    Update AppDelegate.swift

--- roll-a-ball/AppDelegate.swift
+++ roll-a-ball/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
-//        BallPhysicsSystem.registerSystem()
+        BallPhysicsSystem.registerSystem()
         
         // Create the SwiftUI view that provides the window contents.
         let contentView = ContentView()
