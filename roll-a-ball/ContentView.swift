98a734a446c9c7ae92227b9e6c70520b02540b15
Deep Shah <deepshah11111@gmail.com>
2023-12-06 12:08:16 -0800

    Something works

--- roll-a-ball/ContentView.swift
+++ roll-a-ball/ContentView.swift
@@ -7,6 +7,8 @@
 
 import SwiftUI
 import RealityKit
+import Combine
+
 
 enum ForceDirection {
     case up, down, left, right
@@ -79,14 +81,25 @@ struct ARViewContainer: UIViewRepresentable {
         return nil
     }
     
+    func findModelEntity(byName name: String, in entity: Entity) -> Entity? {
+        if entity.name == name {
+            return entity
+        }
+
+        for child in entity.children {
+            if let foundEntity = findModelEntity(byName: name, in: child) {
+                return foundEntity
+            }
+        }
+
+        return nil
+    }
+
+    
     func makeUIView(context: Context) -> ARGameView {
         
         // let arView = ARView(frame: .zero)
         
-        let physics = PhysicsBodyComponent(
-            massProperties: .default, material: .default, mode: .dynamic)
-        
-//        let motion = PhysicsMotionComponent(linearVelocity: [0.1 ,0, 0], angularVelocity: [3, 3, 3])
 
         guard var ballEntity = GameEntities.ball else {
                 fatalError("Failed to load the Bowling Ball USDZ model.")
@@ -98,10 +111,15 @@ struct ARViewContainer: UIViewRepresentable {
             print("Object_0 not found.")
         }
         
-
+        var physics = PhysicsBodyComponent(massProperties: .init(mass: 10.0), material: .generate(friction: 0.4, restitution: 0) , mode: .dynamic)
+        
+        
         ballEntity.components.set(physics)
-//        ballEntity.components.set(motion)
-//        ballEntity.components[PhysicsMotionComponent.self] = PhysicsMotionComponent()
+        
+        // let motion = PhysicsMotionComponent(linearVelocity: [0.1 ,0, 0], angularVelocity: [3, 3, 3])
+        // ballEntity.components.set(motion)
+        // ballEntity.components[PhysicsMotionComponent.self] = PhysicsMotionComponent()
+        // ballEntity.components[PhysicsBodyComponent.self]?.massProperties =
         
         ballEntity.components[BallComponent.self] = BallComponent()
         
@@ -109,62 +127,88 @@ struct ARViewContainer: UIViewRepresentable {
         ballEntity.generateCollisionShapes(recursive: true)
         ballEntity.transform.translation = SIMD3<Float>(0, 0, 0)
         ballEntity.transform.scale = SIMD3<Float>(0.1, 0.1, 0.1)
-//        print("here")
-//        print(ballEntity as? ModelEntity)
-//        print(ballEntity as? HasPhysicsBody)
-        GameEntities.ball = ballEntity
+        
+        // ballEntity.components[ModelComponent.self]?.material?.roughness = 0.5
 
+        GameEntities.ball = ballEntity
+        
         
         // Download From: https://sketchfab.com/3d-models/bowling-pin-028ccb945012460aa9056ffda5b53e20#comments
         guard var pin0 = try? ModelEntity.load(named: "Bowling_Pin.usdz") else {
                 fatalError("Failed to load the Pin USDZ model.")
         }
         
+        var pin_main = pin0
+
         if let object1 = findModelEntity(in: pin0) {
             pin0 = object1
         } else {
             print("Object_1 not found.")
         }
         
-        pin0.components.set(physics)
+        if let object2 = findModelEntity(byName: "Object_1", in: pin_main) {
+            pin0.addChild(object2)
+        } else {
+            print("Can't get child")
+        }
+
+        let physics2 = PhysicsBodyComponent(massProperties: .init(mass: 0.001), material: .generate(friction: 0.5, restitution: 0), mode: .kinematic)
+        
+        pin0.components.set(physics2)
         pin0.generateCollisionShapes(recursive: true)
-//        pin0.orientation = simd_quatf(angle: .pi/2, axis: [0,0,0])
+        pin0.components[PinRotatedComponent.self] = PinRotatedComponent()
+        pin0.orientation = simd_quatf(angle: (.pi * -0.25), axis: [1,0,0])
+        // pin0.orientation = simd_quatf(angle: (.pi * -0.50), axis: [0,0,1])
         
         // Create three additional copies of the original entity
         let pin1 = pin0.clone(recursive: true)
+        pin1.components[PinRotatedComponent.self]?.num = 1
+
         let pin2 = pin0.clone(recursive: true)
+        pin2.components[PinRotatedComponent.self]?.num = 2
+        
         let pin3 = pin0.clone(recursive: true)
+        pin3.components[PinRotatedComponent.self]?.num = 3
+        
         let pin4 = pin0.clone(recursive: true)
+        pin4.components[PinRotatedComponent.self]?.num = 4
+        
 
         // Set positions for the additional pins (adjust positions as needed)
         pin1.transform.translation = SIMD3<Float>(0.0, 0.0, 0.4)
         pin2.transform.translation = SIMD3<Float>(0.0, 0.0, -0.4)
         pin3.transform.translation = SIMD3<Float>(0.4, 0.0, 0.0)
         pin4.transform.translation = SIMD3<Float>(-0.4, 0.0, 0.0)
-        print(pin0)
+        
+        // print(ballEntity, pin1, pin1.isActive)
         
         GameEntities.pins.append(contentsOf: [pin1, pin2, pin3, pin4])
         
+        // print("Components", pin0.components[SynchronizationComponent.self], pin0.components[Transform.self])
+        
         // Create horizontal plane anchor for the content
         let anchor = AnchorEntity(.plane(.horizontal, classification: .any, minimumBounds: SIMD2<Float>(0.2, 0.2)))
-//        anchor.components.set(physics)
-//        anchor.generateCollisionShapes(recursive: true)
+         anchor.components.set(physics)
+         anchor.generateCollisionShapes(recursive: true)
         
 
        // Add collision to the horizontal plane
         let planeMesh = MeshResource.generatePlane(width: 10, depth: 10)
         let planeCollider = ModelEntity(mesh: planeMesh)
-        planeCollider.physicsBody = PhysicsBodyComponent(massProperties: .default, material: .default, mode: .static)
+        planeCollider.physicsBody = PhysicsBodyComponent(massProperties: .init(mass: 0.01), material: .generate(friction: 0.8, restitution: 0), mode: .static)
         planeCollider.generateCollisionShapes(recursive: true)
+        planeCollider.physicsBody?.massProperties = .init(mass: 0.01)
+        // planeCollider.components[PhysicsBodyMode]?.resti
         planeCollider.components[ModelComponent.self] = nil // make the floor invisible
         anchor.addChild(planeCollider)
         
 
         anchor.children.append(contentsOf: [ballEntity, pin1, pin2, pin3, pin4])
-        
-
+    
         // Add the horizontal plane anchor to the scene
         arView.scene.anchors.append(anchor)
+   
+        arView.setupCollisionDetection()
 
         return arView
         
@@ -176,31 +220,78 @@ struct ARViewContainer: UIViewRepresentable {
 
 struct BallComponent: Component {
     var direction: ForceDirection?
+    
+    static let query = EntityQuery(where: .has(BallComponent.self))
+}
+
+struct PinRotatedComponent: Component {
+    var isRotated = false
+    var num = 0
 }
 
 class ARGameView: ARView {
+    private var subscriptions = Set<AnyCancellable>()
+    
     func startApplyingForce(direction: ForceDirection) -> Void {
-//        print("apply force: \(direction.symbol)")
+        // print("apply force: \(direction.symbol)")
         var ballState = GameEntities.ball?.components[BallComponent.self] as? BallComponent
         ballState?.direction = direction
         GameEntities.ball?.components[BallComponent.self] = ballState
     }
     
     func stopApplyingForce() -> Void {
-//        print("force stop")
+        // print("force stop")
         var ballState = GameEntities.ball?.components[BallComponent.self] as? BallComponent
         ballState?.direction = nil
         GameEntities.ball?.components[BallComponent.self] = ballState
     }
+    
+    func setupCollisionDetection() {
+        scene.subscribe(to: CollisionEvents.Began.self) { event in
+            if let ball = GameEntities.ball, let pin = GameEntities.pins.first(where: { $0 == event.entityA || $0 == event.entityB }) {
+                        // Check if both the ball and pin are involved in the collision
+                        if (event.entityA == ball && event.entityB == pin) || (event.entityA == pin && event.entityB == ball) {
+                            self.rotatePinOnCollision(pin: pin)
+                        }
+                    }
+        }.store(in: &subscriptions)
+    }
+
+    // Add this method in the ARGameView class
+    func rotatePinOnCollision(pin: Entity) {
+        if var pinRotatedComponent = pin.components[PinRotatedComponent.self] as? PinRotatedComponent, !pinRotatedComponent.isRotated, let physicsBody = pin as? HasPhysicsBody {
+            
+            var tmpDirection = ForceDirection.right
+            
+            if let direction = (GameEntities.ball?.components[BallComponent.self] as? BallComponent)?.direction {
+                tmpDirection = direction
+                // print("HERE DIRECTION ", tmpDirection.vector)
+            }
+            let v: Float = tmpDirection.vector.x>=0 ? (tmpDirection.vector.z>0 ? 1 : -1) : 1
+            
+            var transform = pin.transform
+            
+            transform.rotation = simd_quatf(angle: (.pi * 0.5) * v, axis: [0, 0, 1]) // Rotate 90 degrees around the x-axis
+                pin.move(to: transform, relativeTo: pin.parent, duration: 0.5, timingFunction: .easeInOut)
+                // pin.orientation = simd_quatf(angle: (.pi * -0.50), axis: [0,0,1])
+            
+                pinRotatedComponent.isRotated = true
+                pin.components[PinRotatedComponent.self] = pinRotatedComponent
+             
+        }
+    }
 }
 
 class BallPhysicsSystem: System {
+    static var pinNeedsUpdate = false
+    
     required init(scene: RealityKit.Scene) { }
     
     func update(context: SceneUpdateContext) {
         if let ball = GameEntities.ball {
             move(ball: ball)
         }
+        
     }
     
     private func move(ball: Entity) {
@@ -212,15 +303,16 @@ class BallPhysicsSystem: System {
         guard let direction = ballComponent.direction else {
             return
         }
-//        print("got phy")
-
-//                let impulseStrength: Float = 0.0005 // Adjust this value based on desired impulse strength
-//                let impulse = direction.vector * impulseStrength
-
-//                physicsBody.addForce(impulse, relativeTo: nil)
-//                ball.components[PhysicsMotionComponent.self] = physicsBody
         
-        let torqueStrength: Float = 0.05  // Adjust this value based on desired rotation strength
+        /* print("got phy")
+
+                let impulseStrength: Float = 0.002 // Adjust this value based on desired impulse strength
+                let impulse = direction.vector * impulseStrength
+
+                physicsBody.applyLinearImpulse(impulse, relativeTo: nil)
+                ball.components[PhysicsMotionComponent.self] = physicsBody */
+        
+        let torqueStrength: Float = 0.02  // Adjust this value based on desired rotation strength
         let torque = SIMD3<Float>(direction.vector.z, 0, -direction.vector.x) * torqueStrength
             physicsBody.addTorque(torque, relativeTo: nil)
     }
@@ -272,6 +364,15 @@ struct JoystickView: View {
         }
     }
 }
+
+
+extension Sequence {
+    var first: Element? {
+        var iterator = self.makeIterator()
+        return iterator.next()
+    }
+}
+
 
 
 #Preview {
